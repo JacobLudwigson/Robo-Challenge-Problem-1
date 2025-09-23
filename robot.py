303654b93ffe99cea45bb5da4eb9f35c2ad64f52
Calvin Bonomo <cal.bonomo@gmail.com>
2025-09-23 14:20:17 -0600

    Added outline code for robot

--- robot.py
+++ robot.py
@@ -1,3 +1,26 @@
 class robot:
-    def __init__(self) -> None:
+    def __init__(self, start_pos: tuple[int, int], goal_pos: tuple[int, int]) -> None:
+        self.current_pos: tuple[int, int] = start_pos
+        self.goal_pos: tuple[int, int] = goal_pos
+        self.next_step = self.calculate_next_step()
+    
+    def get_next_step() -> tuple[int, int]:
+        pass
+
+    def take_step() -> None:
+        pass
+    
+    def calculate_next_step() -> tuple[int, int]:
+        pass
+
+class quadrotor(robot):
+    def __init__(self, start_pos: tuple[int, int], goal_pos: tuple[int, int]) -> None:
+        pass
+
+class differential_drive(robot):
+    def __init__(self, start_pos: tuple[int, int], goal_pos: tuple[int, int]) -> None:
+        pass
+
+class humanoid(robot):
+    def __init__(self, start_pos: tuple[int, int], goal_pos: tuple[int, int]) -> None:
         pass
