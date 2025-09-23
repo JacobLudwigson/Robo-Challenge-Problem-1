3d9a1975884bc92daa618807c3f8ebf462706f1e
JacobLudwigson <jalu1895@colorado.edu>
2025-09-23 14:40:31 -0600

    Pushing init funciton for warehouse

--- warehouse.py
+++ warehouse.py
@@ -1,5 +1,36 @@
+import numpy as np
+import random
+import robot
+
 class warehouse:
-    def __init__(self,gridSize) -> None:
+    robots = []
+    numRobots = 0
+    def __init__(self,gridSize, numRobots) -> None:
+        self.numRobots = numRobots
         self.gridSize = gridSize
         self.grid = [[0] * gridSize]*gridSize
-    
+        for i in range(0, numRobots):
+            #Get a random position and regenerate if not
+            startX,startY = random.randint(1, gridSize), random.randint(1, gridSize)
+            while (gridSize[startX][startY]):
+                startX,startY = random.randint(1, gridSize), random.randint(1, gridSize)
+            
+            goalX,goalY = random.randint(1, gridSize), random.randint(1, gridSize)
+            robotType = random.randint(1,3)
+            if robotType == 1:
+                self.robots.append(quadrotor((startX,startY), (goalX,goalY)))
+            elif robotType == 2:
+                self.robots.append(differential_drive((startX,startY), (goalX,goalY)))
+            else:
+                self.robots.append(humanoid((startX,startY), (goalX,goalY)))
+
+    def timeStep(self):
+        nextSteps = []
+        for i in self.robots:
+            nextSteps.append(i.next_step)
+        for i in self.robots:
+        
+            
+
+        
+
